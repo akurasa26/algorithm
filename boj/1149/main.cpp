d0cb82897285d3004fcf2273498492d14d89727b
Hyeon-Mook Jerry Choi <chm073@gmail.com>
2019-11-24 01:39:38 +0900

    Reformat

--- boj/1149/main.cpp
+++ boj/1149/main.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 int d[1000][3];
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -12,15 +13,14 @@ int main() {
     int n;
     cin >> n;
 
-    for(int i = 1; i <= n; i++) {
-
+    for (int i = 1; i <= n; i++) {
         cin >> d[i][0];
         cin >> d[i][1];
         cin >> d[i][2];
 
-        d[i][0] = min(d[i-1][1], d[i-1][2]) + d[i][0];
-        d[i][1] = min(d[i-1][2], d[i-1][0]) + d[i][1];
-        d[i][2] = min(d[i-1][1], d[i-1][0]) + d[i][2];
+        d[i][0] = min(d[i - 1][1], d[i - 1][2]) + d[i][0];
+        d[i][1] = min(d[i - 1][2], d[i - 1][0]) + d[i][1];
+        d[i][2] = min(d[i - 1][1], d[i - 1][0]) + d[i][2];
     }
 
     cout << min(min(d[n][0], d[n][1]), d[n][2]);
