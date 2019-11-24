352084fb2d28123c58094eb2fcdb1ed3c8c166b0
Hyeon-Mook Jerry Choi <chm073@gmail.com>
2019-11-24 23:34:03 +0900

    Add 10869

---
+++ boj/10869/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by jelly on 24/11/2019.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+
+    return 0;
+}
