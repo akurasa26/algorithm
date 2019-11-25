0249dcff9cec85df90809ece9cdc2efb1b8b5188
jelly <chm073@gmail.com>
2019-11-25 23:26:17 +0900

    Add 8393

---
+++ boj/8393/main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by jelly on 25/11/2019.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int sum = 0;
+    for(int i = 1; i <=n; i++) {
+        sum += i;
+    }
+
+    cout << sum;
+    return 0;
+}
