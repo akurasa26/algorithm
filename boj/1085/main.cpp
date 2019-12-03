7b06f834c6efef2314c03d47855ebc74440a4a48
jelly <chm073@gmail.com>
2019-12-03 23:39:25 +0900

    Add 1085

---
+++ boj/1085/main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by jelly on 03/12/2019.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y, w, h;
+    cin >> x >> y >> w >> h;
+    int min = x;
+
+    if (min > y) min = y;
+    if (min > (w - x)) min = w - x;
+    if (min > (h - y)) min = h - y;
+
+    cout << min;
+    return 0;
+}
