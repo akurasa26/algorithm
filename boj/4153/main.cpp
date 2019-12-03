0c6e11923ef01115770111005b9aafe05e3a732d
jelly <chm073@gmail.com>
2019-12-04 00:11:24 +0900

    Add 4153

--- boj/4153/main.cpp
+++ boj/4153/main.cpp
@@ -2,15 +2,19 @@
 // Created by jelly on 03/12/2019.
 //
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
     int a, b, c;
+    int M;
+    int sum;
     while (1) {
         cin >> a >> b >> c;
-        int M = max(max(a, b), c);
-        int sum = 0;
+        if (a == 0 && b == 0 && c == 0) break;
+        M = max(max(a, b), c);
+        sum = 0;
         if (a != M)
             sum += (a * a);
         if (b != M)
@@ -18,6 +22,9 @@ int main() {
         if (c != M)
             sum += (c * c);
 
+        if ((sqrt(sum) * sqrt(sum)) == (M * M)) cout << "right";
+        else cout << "wrong";
+        cout << '\n';
     }
 
     return 0;
