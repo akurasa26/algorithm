160b144f5bd67311379a3d06e6bb03de4dc1c973
jelly <chm073@gmail.com>
2019-12-19 22:41:36 +0900

    Add 11651

---
+++ boj/11651/main.cpp
@@ -0,0 +1,50 @@
+//
+// Created by jelly on 19/12/2019.
+//
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+struct Point {
+    int x;
+    int y;
+};
+vector<Point> v;
+
+bool compare(Point &a, Point &b) {
+    if (a.y == b.y) {
+        if (a.x < b.x) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if (a.y < b.y) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        Point p;
+        cin >> p.x >> p.y;
+        v.push_back(p);
+    }
+    sort(v.begin(), v.end(), compare);
+
+    for (int i = 0; i < n; i++) {
+        cout << (v[i].x) << ' ' << v[i].y << '\n';
+
+    }
+    return 0;
+}
