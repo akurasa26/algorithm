23297081412e9e64a1dbc85ccbd121cb71a97879
jelly <chm073@gmail.com>
2019-12-26 23:06:19 +0900

    Update 2579

--- boj/2579/main.cpp
+++ boj/2579/main.cpp
@@ -2,7 +2,6 @@
 // Created by jelly on 25/12/2019.
 //
 #include <iostream>
-
 using namespace std;
 int a[301];
 int d[301][2];
@@ -17,9 +16,10 @@ int main() {
     // 0 이 이전수를 안골랐을 경우
     // 1 은 이전수를 골랐을 경우
     d[1][0] = a[1];
+    d[1][1] = a[1];
     for (int i = 2; i <= t; i++) {
         d[i][0] = d[i - 2][1] + a[i];
-        d[i][1] = d[i - 1][0] + a[i];
+        d[i][1] = max(d[i - 1][0] + a[i], d[i - 2][1] + a[i]);
     }
 
     cout << max(d[t][0], d[t][1]);
