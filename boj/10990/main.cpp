927043545e341053a14520f63343534ef8b4bc85
jelly <chm073@gmail.com>
2020-01-02 23:37:08 +0900

    Add 10990

---
+++ boj/10990/main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by jelly on 02/01/2020.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j < n - i; j++) cout << ' ';
+        cout << '*';
+
+        for (int j = 0; j < (i * 2) - 1; j++) cout << ' ';
+        if (i != 0)cout << '*';
+        cout << '\n';
+    }
+
+    return 0;
+}
