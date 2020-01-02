2a0fa96f5a9d813ff96dede3e23b56e3a7a5dc27
jelly <chm073@gmail.com>
2020-01-02 22:58:21 +0900

    Add 2442

---
+++ boj/2442/main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by jelly on 02/01/2020.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = n - i; j > 0; j--) cout << ' ';
+        for (int j = 0; j < i * 2 - 1; j++) cout << '*';
+
+        cout << '\n';
+    }
+
+    return 0;
+}
+
