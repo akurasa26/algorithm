f4bf557b5f930d09f6c102385d429443be8bbaa8
jelly <chm073@gmail.com>
2020-01-04 23:29:29 +0900

    Add 2565

--- boj/2565/main.cpp
+++ boj/2565/main.cpp
@@ -3,66 +3,40 @@
 //
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
+int a[500 + 1];
 
-int a[501];
-int idx[501];
-int ans = 999999999;
-int t;
-
-
-bool check() {
-    for (int j = 0; j < t; j++) {
-        int i = idx[j];
-        if (a[i] == 0) continue;
-        for (int k = i - 1; k >= 0; k--) {
-            if (a[k] > a[i]) return false;
-            if (a[k] == 0) continue;
-            if (a[k] < a[i]) break;
-        }
-    }
-    return true;
-}
-
-void go(int j, int c) {
-    if (j == t) return;
-    if (c >= ans) return;
-
-    for (int k = j; k < t; k++) {
-        int m = idx[k];
-        int temp = a[m];
-        a[m] = 0;
-        if (check()) {
-            ans = min(c, ans);
-            a[m] = temp;
-            return;
-        }
-        go(k + 1, c + 1);
-        a[m] = temp;
-    }
-    go(j + 1, c);
-}
-
+int d[500 + 1];
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    int n;
+    cin >> n;
 
-    cin >> t;
-    for (int i = 0; i < t; i++) {
-        int b, c;
-        cin >> b >> c;
-        a[b] = c;
-        idx[i] = b;
+    for (int i = 0; i < n; i++) {
+        int l, r;
+        cin >> l >> r;
+        a[l] = r;
     }
 
-    sort(idx, idx + t);
-
-    if (!check()) {
-        go(0, 1);
+    for (int i = 1; i <= 500; i++) {
+        if (a[i]) {
+            d[i] = 1;
+            int l = i;
+            int r = a[i];
+            for (int j = l - 1; j > 0; j--) {
+                if (a[j] && a[j] < a[i]) {
+                    d[i] = max(d[j] + 1, d[i]);
+                }
+            }
+        }
     }
 
-    cout << ans;
+    int M = 0;
+    for (int i = 1; i <= 500; i++) {
+        M = max(M, d[i]);
+    }
+
+    cout << n - M;
+
     return 0;
 }
