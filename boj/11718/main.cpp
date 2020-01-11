eaf2b8bd7634e5fa93259e73d36b67a5d767a2bf
jelly <chm073@gmail.com>
2020-01-11 23:46:49 +0900

    Add 11718 (still failed)

---
+++ boj/11718/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by jelly on 11/01/2020.
+//
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+
+int main() {
+    while (true) {
+        char str[101];
+        cin.getline(str, 101);
+        cout << str << '\n';
+    }
+
+    return 0;
+}
