b5c0a2c6b3115456c5117a9afbd8345d480013a7
jelly <chm073@gmail.com>
2020-01-12 22:28:20 +0900

    Add 10039 (still failed)

--- boj/11718/main.cpp
+++ boj/11718/main.cpp
@@ -1,18 +1,14 @@
 //
 // Created by jelly on 11/01/2020.
 //
-#include <iostream>
+#include <cstdio>
 #include <cstring>
-
 using namespace std;
-
-
 int main() {
-    while (true) {
-        char str[101];
-        cin.getline(str, 101);
-        cout << str << '\n';
+    char str[103];
+    while (fgets(str, 103, stdin) != NULL) {
+        str[strlen(str) - 1] = '\0';
+        printf("%s\n", str);//fgets는 공백을 포함.
     }
-
     return 0;
 }
