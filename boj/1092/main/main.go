793bc7da678b2d3c33103f107e8df4be170de438
jelly <chm073@gmail.com>
2021-09-02 23:25:48 +0900

    By checker

--- boj/1092/main/main.go
+++ boj/1092/main/main.go
@@ -1,5 +1,69 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
 
+func main() {
+	var crains []int
+	var n, m int
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Split(bufio.ScanWords)
+	sc.Scan()
+	n, _ = strconv.Atoi(sc.Text())
+	for i := 0; i < n; i++ {
+		sc.Scan()
+		b, _ := strconv.Atoi(sc.Text())
+		crains = append(crains, b)
+	}
+
+	sc.Scan()
+	m, _ = strconv.Atoi(sc.Text())
+	var boxes []int
+	for i := 0; i < m; i++ {
+		sc.Scan()
+		b, _ := strconv.Atoi(sc.Text())
+		boxes = append(boxes, b)
+	}
+
+	sort.Ints(crains)
+	crainPos := make([]int, n)
+	boxCheck := make([]bool, m)
+	for i, j := 0, len(crains)-1; i < j; i, j = i+1, j-1 {
+		crains[i], crains[j] = crains[j], crains[i]
+	}
+	sort.Ints(boxes)
+
+	for i, j := 0, len(boxes)-1; i < j; i, j = i+1, j-1 {
+		boxes[i], boxes[j] = boxes[j], boxes[i]
+	}
+	count := 0
+
+	if boxes[0] > crains[0] {
+		fmt.Printf("%d", -1)
+		return
+	}
+	checked := 0
+	for checked < m {
+		for i := range crains {
+			for j := crainPos[i]; j < m; j++ {
+				if !boxCheck[j] && crains[i] >= boxes[j] {
+					crainPos[i] = j + 1
+					boxCheck[j] = true
+					checked++
+					break
+				}
+			}
+		}
+		if count > 10000 {
+			fmt.Printf("%d", -1)
+			return
+		}
+		count++
+	}
+	fmt.Printf("%d", count)
 }
